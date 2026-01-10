696bf6a47fb5edff046e95d20e66dac65c92cf3e
aungkyawth3t <aungkyawthethimself@gmail.com>
2026-01-10 22:36:11 +0630

    update

--- test.php
+++ test.php
@@ -13,18 +13,18 @@
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="test.css?v=<?= time() ?>" />
+  <link rel="stylesheet" href="styles/test.css?v=<?= time() ?>" />
 </head>
 
 <body>
   <div class="container-fluid">
     <div class="row">
-      <?php include __DIR__ . '/sidebar.php'; ?>
+      <?php include __DIR__ . '/components/sidebar.php'; ?>
       <!-- Main -->
       <main class="col-lg-10 right-container">
-        <?php include __DIR__ . '/nav.php'; ?>
-        <?php include __DIR__ . '/header.php'; ?>
-        <?php include __DIR__ . '/task-list.php'; ?>
+        <?php include __DIR__ . '/components/nav.php'; ?>
+        <?php include __DIR__ . '/components/header.php'; ?>
+        <?php include __DIR__ . '/components/task-list.php'; ?>
       </main>
     </div>
   </div>
